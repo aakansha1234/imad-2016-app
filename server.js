85ec84ee9e2f05a378559f925c183b17931ffd7f
aakansha1234 <aakanshag27@gmail.com>
2016-09-18 11:48:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,17 @@ app.get('/article-one', function(req,res)
     res.sendFile(path.join(__dirname, 'ui', 'article-one .html'));
     
 });
+app.get('/article-two', function(req,res)
+{
+    res.sendFile(path.join(__dirname, 'ui', 'article-one .html'));
+    
+});
+app.get('/article-three', function(req,res)
+{
+    res.sendFile(path.join(__dirname, 'ui', 'article-one .html'));
+    
+});
+
 
 app.get('/article-two', function(req,res)
 {
